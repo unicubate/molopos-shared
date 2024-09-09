d6d300c5879a9ecd1d5d03a8fff4a94e4bcb4d69
cieffelle <lorenzo.cicuttin@recrowd.com>
2024-09-09 09:23:35 +0200

    onfido status

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from "./cities"
-export * from "./countries"
-export * from "./lemonway"
+export * from "./cities";
+export * from "./countries";
+export * from "./lemonway";
+export * from "./onfido";
