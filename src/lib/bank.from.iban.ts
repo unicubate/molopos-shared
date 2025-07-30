ebac3ae66900fc62d126404ce803c9f3958ecee1
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2025-07-30 15:22:38 +0200

    add iban constraints

--- src/lib/bank.from.iban.ts
+++ src/lib/bank.from.iban.ts
@@ -1,6 +1,7 @@
 import { itBanks } from "../data/it.banks";
 
 export function getBankNameFromIban(iban: string) {
+  if (!iban.startsWith("IT") || iban.length !== 27) return null;
   const bankCode = iban.slice(5, 10);
   const codeWithoutLeadingZeroes = Number(bankCode).toString();
   const bank = itBanks.find((b) => b.code === codeWithoutLeadingZeroes);
