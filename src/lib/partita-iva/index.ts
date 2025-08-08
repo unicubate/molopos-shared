37d463688e86541f7a40ccc6f9b0d7ac7e9381e4
prahbdeep0multani <prabhdeep.multani@recrowd.com>
2025-08-08 12:34:11 +0200

    adds check fn  for p.iva

---
+++ src/lib/partita-iva/index.ts
@@ -0,0 +1,47 @@
+export function checkPartitaIVA(piva: string): boolean {
+    if (!/^\d{11}$/.test(piva)) return false;
+
+    let sum = 0;
+
+    for (let i = 0; i < 10; i++) {
+        let digit = parseInt(piva.charAt(i), 10);
+
+        if (i % 2 === 0) {
+            sum += digit;
+        } else {
+            digit *= 2;
+            if (digit > 9) digit -= 9;
+            sum += digit;
+        }
+    }
+
+    const expectedCheckDigit = (10 - (sum % 10)) % 10;
+    const actualCheckDigit = parseInt(piva.charAt(10), 10);
+
+    return expectedCheckDigit === actualCheckDigit;
+}
+
+export type PartitaIVAData = {
+    numero: string;
+    isForeign: boolean;
+};
+
+/**
+ * @throws if the partita IVA is invalid
+ */
+export function parsePartitaIVA(piva: string): PartitaIVAData {
+    piva = piva.trim();
+
+    if (!checkPartitaIVA(piva)) {
+        throw new Error("Invalid Partita IVA");
+    }
+
+    // In alcune convenzioni, i numeri da 0000001 a 1000000 sono italiani.
+    // Quelli da 1000001 in poi potrebbero essere attribuiti a soggetti esteri.
+    const isForeign = parseInt(piva.substring(0, 7), 10) >= 1000000;
+
+    return {
+        numero: piva,
+        isForeign
+    };
+}
