3da9e13aa9b58ea67d17aaede65b098c7164f8e8
boclair237 <boclair.temgoua@recrowd.com>
2026-05-02 10:23:09 +0200

    chore: update package version to 2.0.9 and enhance utility tests

--- src/lib/utils/index.test.ts
+++ src/lib/utils/index.test.ts
@@ -4,20 +4,24 @@ describe("Utils", () => {
   test("fromCents", () => {
     const result = fromCents(100);
     expect(result).toEqual(1);
+    expect(result).not.toBeNull();
   });
 
   test("toCents", () => {
     const result = toCents(1);
     expect(result).toEqual(100);
+    expect(result).not.toBeNull();
   });
 
   test("calculVat", () => {
     const result = calculVat({ value: 100, percentage: 20 });
     expect(result).toEqual(20);
+    expect(result).not.toBeNull();
   });
 
   test("calculDiscount", () => {
     const result = calculDiscount({ value: 100, percentage: 20 });
     expect(result).toEqual(20);
+    expect(result).not.toBeNull();
   });
 });
